d5d8d122ee3d014f5278a442489beff5b79ea4e2
Fenysk <minefenysk@gmail.com>
2024-02-15 14:11:11 +0100

    🐛 Platform can't be deleted if linked items

--- src/platforms/platforms.service.ts
+++ src/platforms/platforms.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { Platform } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePlatformDto } from './dto/create-platform.dto';
@@ -8,7 +8,11 @@ export class PlatformsService {
     constructor(private readonly prismaService: PrismaService) { }
 
     async getAllPlatforms(): Promise<Platform[]> {
-        const platforms = await this.prismaService.platform.findMany();
+        const platforms = await this.prismaService.platform.findMany({
+            include: {
+                Items: true
+            }
+        });
 
         if (!platforms.length)
             throw new NotFoundException('No platforms found');
@@ -23,13 +27,19 @@ export class PlatformsService {
     }
 
     async deletePlatform(platformId: string): Promise<string> {
-
         const platform = await this.prismaService.platform.findUnique({
-            where: { platformId }
+            where: { platformId },
+            select: {
+                name: true,
+                Items: true
+            }
         });
 
         if (!platform)
             throw new NotFoundException('Platform not found');
+
+        if (platform.Items.length)
+            throw new ConflictException('Platform has items, cannot delete');
 
         await this.prismaService.platform.delete({
             where: { platformId }
